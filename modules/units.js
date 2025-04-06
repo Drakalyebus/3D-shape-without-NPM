cc4c40ee5f65b26751d36f438e74ddf81a3b9aec
Drakalyebus <mishastelephonom@gmail.com>
2025-04-07 01:20:04 +0300

    v1.0.1

--- modules/units.js
+++ modules/units.js
@@ -23,8 +23,9 @@ class Units {
         const project = (x, y, z) => Perspective.projecting(x, y, z, size, dist, perspective, canvas.width / 2, canvas.height / 2);
         const units = this.units.sort((a, b) => Math.max(...b.faces.map(face => Math.max(...face.map(point => point.z)))) - Math.max(...a.faces.map(face => Math.max(...face.map(point => point.z)))));
         units.forEach(unit => {         
-            const faces = unit.faces.sort((a, b) => Math.max(...b.map(point => point.z)) - Math.max(...a.map(point => point.z)));
+            // const faces = unit.faces.sort((a, b) => (Math.max(...b.map(point => point.z)) + Math.min(...b.map(point => point.z)) / 2) - (Math.max(...a.map(point => point.z)) / 2));
             // const faces = unit.faces.sort((a, b) => b.filter(point => point.z > Math.max(...b.map(point => point.z))).length - a.filter(point => point.z > Math.max(...b.map(point => point.z))).length);
+            const faces = unit.faces.sort((a, b) => b.reduce((sum, point) => sum + point.z, 0) / b.length - a.reduce((sum, point) => sum + point.z, 0) / a.length);
             faces.forEach(face => {
                 const prePoints = face;
                 const points = face.map(point => project(point.x, point.y, point.z));
@@ -37,7 +38,7 @@ class Units {
                 ctx.lineTo(points[0].x, points[0].y);
                 ctx.closePath();
                 const normal = Vector.normal(new Vector(prePoints[0].x - prePoints[1].x, prePoints[0].y - prePoints[1].y, prePoints[0].z - prePoints[1].z), new Vector(prePoints[1].x - prePoints[2].x, prePoints[1].y - prePoints[2].y, prePoints[1].z - prePoints[2].z)).normalize();
-                const brightness = shadows ? Math.min(1, Math.max(0, Vector.reflect(normal, light).prod)) : 1;
+                const brightness = shadows ? Math.min(1, Math.max(0, Vector.reflect(normal, light).prod * intensity)) : 1;
                 ctx.fillStyle = `rgba(${unit.color.r * brightness}, ${unit.color.g * brightness}, ${unit.color.b * brightness}, ${unit.color.a})`;
                 ctx.strokeStyle = `rgba(${unit.outline.r}, ${unit.outline.g}, ${unit.outline.b}, ${unit.outline.a})`;
                 ctx.stroke();
